6642dec43e7651c32febe76c3899633457868d6f
kPriyank07 <priyank25csu352@ncuindia.edu>
2026-02-09 00:12:22 +0530

    Create Q20(b).cpp

---
+++ Q20(b).cpp
@@ -0,0 +1,17 @@
+/* 5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=5;i>=1;i--){
+        for(int j=5;j>=6-i;j--)
+            cout<<j<<" ";
+        cout<<endl;
+    }
+}
